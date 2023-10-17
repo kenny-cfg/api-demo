03dc4f5ca28a54bb9860b3629750e5bb814b7323
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-10-17 20:25:40 +0100

    Another endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flights import flights
 
 
@@ -14,14 +14,23 @@ def ping():
 def get_flights():
     return jsonify(flights)
 
+
 @app.route('/flights/by_from_city/<string:from_city>')
 def get_single_flight_by_from_city(from_city):
     flight = [element for element in flights if element['from_city'] == from_city]
     return flight
 
+
 @app.route('/flights/<int:id>')
 def get_single_flight(id):
     flight = [element for element in flights if element['flight_id'] == id]
+    return flight
+
+
+@app.route('/flights', methods=['POST'])
+def add_flight():
+    flight = request.get_json()
+    flights.append(flight)
     return flight
 
 
